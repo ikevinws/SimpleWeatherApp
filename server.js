c4a48866db7cfca48d24f8701ad580471037832b
ikevinws <ikevinwang@ufl.edu>
2020-06-24 01:58:53 -0400

    refactor index and server

--- server.js
+++ server.js
@@ -11,15 +11,10 @@ app.use(express.static('public'))
 
 app.post('/weather', (req, res) =>{
     const url=`https://api.openweathermap.org/data/2.5/weather?lat=${req.body.latitude}&lon=${req.body.longitude}&appid=${OPENWEATHER_API_KEY}`
-
-    axios({
-        url: url,
-        responseType: 'json'
-    }).then(data => {
-        return res.json(data.data)
-    }).catch((error) => {
-        console.error(error);
-    });
+    
+    axios.get(url)
+        .then(data=>res.json(data.data))
+        .catch((error)=>console.log(error))
 })
 
 app.listen(3000, ()=>{
